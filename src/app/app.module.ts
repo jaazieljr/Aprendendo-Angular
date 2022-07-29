32fd01803dbeabd796f51f4424f65f45e3f846fa
jaazieljr <jaaziel.dev@gmail.com>
2022-07-28 22:51:28 -0300

    primaira parte

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { CourseListComponet } from './course/course-list.components';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseListComponet
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
