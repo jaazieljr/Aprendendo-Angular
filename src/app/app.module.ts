cea197fe07012d370bc08f7bd31694f0a3593184
jaazieljr <jaaziel.dev@gmail.com>
2022-08-08 00:19:29 -0300

    atualizacoes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CourseListComponet } from './course/course-list.components';
+import { StarComponent } from './star/star.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponet
+    CourseListComponet,
+    StarComponent
   ],
   imports: [
     BrowserModule,
--- src/app/course/course-list.components.ts
+++ src/app/course/course-list.components.ts
@@ -15,21 +15,21 @@ export class CourseListComponet implements OnInit {
             {
             id: 1,
             name: 'Angular: Forms',
-            imageUrl: '',
+            imageUrl: './assets/images/forms.png',
             price: 99.99,
             code: 'XPS-8796',
             duration: 120,
-            rating: 5.4,
+            rating: 5,
             releaseDate: 'November, 2, 2019'
             },
             {
                 id: 2,
                 name: 'Angular: HTTP',
-                imageUrl: '',
+                imageUrl: './assets/images/http.png',
                 price: 99.99,
                 code: 'NKL-1094',
                 duration: 80,
-                rating: 4,
+                rating: 4.5,
                 releaseDate: 'December, 4, 2019'
                 }
         ]
